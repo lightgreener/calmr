1666dda57553a19303091302457dae05fc782e10
YOUR_FULL NAME_HERE <your@email.here.com>
2020-11-09 15:50:03 +1100

    Json problem fixed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,7 +30,7 @@ class UsersController < ApplicationController
     respond_to do |format|
       if @user.save
         format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
+        format.json { render :json => @user}
       else
         format.html { render :new }
         format.json { render json: @user.errors, status: :unprocessable_entity }
