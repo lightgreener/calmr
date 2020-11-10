a9b825a8de47e46bf780916ec4a6b8e5d029d97c
YOUR_FULL NAME_HERE <your@email.here.com>
2020-11-10 15:02:48 +1100

    session fixed

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
     root :to => 'pages#new'
 
      # Session routes to allow login and logout
-     post '/login', to: 'sessions#create'
-     delete '/logout', to: 'sessions#destroy'
-     get '/logged_in', to: 'sessions#is_logged_in?'
+     post '/login', to: 'session#create'
+     delete '/logout', to: 'session#destroy'
+     get '/logged_in', to: 'session#is_logged_in?'
 
     resources :users, only: [:create, :show, :index]
     resources :articles
